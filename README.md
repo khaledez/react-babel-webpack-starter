3a127a99c41b27832d104055db516ec38ac0e970
Khaled Ez Zughayyar <ezzughayyar@gmail.com>
2019-11-03 07:26:33 -0500

    experiment with Reakit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Starter Project Using React, Webpack, Babel and ReaKit
+
+This is a really simple setup for react-based projects.  
+
+The main structure is inspired from [How to set up React, webpack, and Babel 7 from scratch](https://www.valentinog.com/blog/babel/)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import FormContainer from "./js/components/container/FormContainer.jsx";
+import { Provider } from "reakit";
+import * as system from "reakit-system-bootstrap";
+import {
+    unstable_useFormState as useFormState,
+    unstable_Form as Form,
+    unstable_FormLabel as FormLabel,
+    unstable_FormInput as FormInput,
+    unstable_FormMessage as FormMessage,
+    unstable_FormSubmitButton as FormSubmitButton
+} from "reakit/Form";
 
-
-const wrapper = document.getElementById("create-article-form");
-wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
+function Example() {
+    const form = useFormState({
+        values: { name: "" },
+        onValidate: values => {
+            if (!values.name) {
+                const errors = {
+                    name: "How can we be friends without knowing your name?"
+                };
+                throw errors;
+            }
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+    return (
+        <Provider unstable_system={system}>
+            <Form {...form}>
+                <FormLabel {...form} name="name">
+                    Name
+        </FormLabel>
+                <FormInput {...form} name="name" placeholder="John Doe" />
+                <FormMessage {...form} name="name" />
+                <FormSubmitButton {...form}>Submit</FormSubmitButton>
+            </Form>
+        </Provider>
+    );
+}
+const wrapper = document.getElementById("root");
+wrapper ? ReactDOM.render(<Example />, wrapper) : false;
