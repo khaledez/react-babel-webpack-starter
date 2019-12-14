ef6edb010a9a5fd7260c1c84c3b9e9b4883cbef2
Khaled Ez Zughayyar <ezzughayyar@gmail.com>
2019-12-13 23:48:04 -0500

    use material-ui

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "reakit";
-import * as system from "reakit-system-bootstrap";
-import {
-    unstable_useFormState as useFormState,
-    unstable_Form as Form,
-    unstable_FormLabel as FormLabel,
-    unstable_FormInput as FormInput,
-    unstable_FormMessage as FormMessage,
-    unstable_FormSubmitButton as FormSubmitButton
-} from "reakit/Form";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
-function Example() {
-    const form = useFormState({
-        values: { name: "" },
-        onValidate: values => {
-            if (!values.name) {
-                const errors = {
-                    name: "How can we be friends without knowing your name?"
-                };
-                throw errors;
-            }
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        }
-    });
-    return (
-        <Provider unstable_system={system}>
-            <Form {...form}>
-                <FormLabel {...form} name="name">
-                    Name
-        </FormLabel>
-                <FormInput {...form} name="name" placeholder="John Doe" />
-                <FormMessage {...form} name="name" />
-                <FormSubmitButton {...form}>Submit</FormSubmitButton>
-            </Form>
-        </Provider>
-    );
-}
+const App = () => (
+    <div>
+        <h1>Title</h1>
+        <Button variant="contained" color="primary">
+            Hello World
+    </Button>
+    </div>
+);
+
 const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<Example />, wrapper) : false;
+wrapper ? ReactDOM.render(<App />, wrapper) : false
